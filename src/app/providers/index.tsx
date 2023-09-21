7a4f48cf798db7ff674ce3bddaeb32caf0372d86
sieugene <sieugene@mail.ru>
2023-09-21 18:56:41 +0900

    chore : prettier, eslint, next-ui, styled components

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,18 @@
+import { NextUIProvider } from "@nextui-org/react";
+import React, { FC } from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#111",
+    secondary: "#0070f3",
+  },
+};
+
+export const Providers: FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <NextUIProvider>{children}</NextUIProvider>
+    </ThemeProvider>
+  );
+};
